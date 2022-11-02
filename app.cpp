708127c728a86923bf38e2802927f662480a4242
Kalev <kallikalev@gmail.com>
2022-11-01 23:25:25 -0400

    adapted glPlay and set up rectangular prism rendering

--- app.cpp
+++ app.cpp
@@ -2,15 +2,24 @@
 #include <GLFW/glfw3.h>
 #include "Shader.h" // our custom shader class
 #include "Camera.h" // our custom camera class
-#include <stb/stb_image.h>
+// #include <stb/stb_image.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <string>
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
+
+struct part {
+    glm::vec3 dimensions; // length, width, height of rectangular prism
+    glm::vec3 position;
+    glm::vec3 rotationAxis;
+    float rotation;
+    glm::vec3 color; // 0-1 rgb
+};
 
 // declare these at the top so that code below can use them, but we can implement at bottom of code
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -28,22 +37,25 @@ float mouseLastX = SCR_WIDTH / 2.0f;
 float mouseLastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
-glm::vec3 lightPos(1.2f,1.0f,2.0f);
-
 // timing
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
 
 int main()
 {
+    // initialize and configure GLFW
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // tell glfw that we're using opengl 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // tell glfw we're using the core profile
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // required on mac
-  
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Check Out This Window", NULL, NULL); // creates a window with glfw, stores it as GLFWwindow object
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // create the GLFW window
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Hexapod Simulator", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -64,23 +76,22 @@ int main()
         return -1;
     }
 
-    glViewport(0, 0, 800, 600); // tell opengl the window size is the same as the glfw window
-
-
+    // configure global opengl state
+    glEnable(GL_DEPTH_TEST);
 
     // build and compile our shader program
     Shader lightingShader("shaders/shader.vs", "shaders/shader.fs");
-    Shader lightCubeShader("shaders/light_cube.vs","shaders/light_cube.fs");
 
     // set up vertex data and buffers and configure vertex attribute
 
-    float vertices[] = { // the 36 vertices for the 12 triangles for the 6 faces of a cube, along with their normals
+    float vertices[] = { // vertices for 36 points of 12 triangles of 6 faces of cube and normals
+        // positions          // normals
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 
         -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
         0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
@@ -117,57 +128,107 @@ int main()
         -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
     };
-    // unsigned int indices[] = {
-    //     0, 1, 3, // front face
-    //     2, 1, 3
-    // };
 
-    unsigned int VBO, cubeVAO/*, EBO*/; // create Vertex Buffer Object, Vertex Array Object, and Element Buffer Object
+    std::vector<part> parts = {
+        // body
+        {
+            glm::vec3(3.0f,0.5f,2.0f), // dimensions
+            glm::vec3(0.0f,0.0f,0.0f), // position
+            glm::vec3(1.0f,0.0f,0.0f), // rotation axis
+            0.0f, // amount of rotation (in degrees)
+            glm::vec3(0.0f,1.0f,1.0f) // color
+        },
+
+        // leg 1
+        {
+            glm::vec3(2.0f,0.5f,0.5f),
+            glm::vec3(1.25f,0.0f,2.0f),
+            glm::vec3(0.0f,1.0f,0.0f),
+            glm::pi<float>()/2,
+            glm::vec3(1.0f,0.2f,0.2f)
+        },
+
+        // leg 2
+        {
+            glm::vec3(2.0f,0.5f,0.5f),
+            glm::vec3(0.0f,0.0f,2.0f),
+            glm::vec3(0.0f,1.0f,0.0f),
+            glm::pi<float>()/2,
+            glm::vec3(1.0f,0.2f,0.2f)
+        },
+
+        // leg 3
+        {
+            glm::vec3(2.0f,0.5f,0.5f),
+            glm::vec3(-1.25f,0.0f,2.0f),
+            glm::vec3(0.0f,1.0f,0.0f),
+            glm::pi<float>()/2,
+            glm::vec3(1.0f,0.2f,0.2f)
+        },
+
+        // leg 4
+        {
+            glm::vec3(2.0f,0.5f,0.5f),
+            glm::vec3(1.25f,0.0f,-2.0f),
+            glm::vec3(0.0f,1.0f,0.0f),
+            glm::pi<float>()/2,
+            glm::vec3(1.0f,0.2f,0.2f)
+        },
+
+        // leg 5
+        {
+            glm::vec3(2.0f,0.5f,0.5f),
+            glm::vec3(0.0f,0.0f,-2.0f),
+            glm::vec3(0.0f,1.0f,0.0f),
+            glm::pi<float>()/2,
+            glm::vec3(1.0f,0.2f,0.2f)
+        },
+
+        // leg 6
+        {
+            glm::vec3(2.0f,0.5f,0.5f),
+            glm::vec3(-1.25f,0.0f,-2.0f),
+            glm::vec3(0.0f,1.0f,0.0f),
+            glm::pi<float>()/2,
+            glm::vec3(1.0f,0.2f,0.2f)
+        }
+    };
+
+
+
+    unsigned int VBO, cubeVAO; // create Vertex Buffer Object, Vertex Array Object
     
     glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &VBO);
-
-    glBindVertexArray(cubeVAO);
 
     // copy vertex array into vertex buffer for OpenGl to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // copy data from vertices to buffer
 
+    glBindVertexArray(cubeVAO);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // position attribute
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // normal attribute
     glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
-    glEnableVertexAttribArray(0);
+ 
 
-
-    // create the light with the same vertices as the cube object
-    unsigned int lightCubeVAO;
-    glGenVertexArrays(1, &lightCubeVAO);
-    glBindVertexArray(lightCubeVAO);
-    // we only need to bind the the existing VBO, it already contians cube data
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);
     // set the vertex attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6  * sizeof(float), (void*)0); // we don't want to use the normals, but update the stride to match
     glEnableVertexAttribArray(0);
 
     // draw as wireframes rather than filled
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    // tell openGl to do depth testing
-    glEnable(GL_DEPTH_TEST);  
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
-    int width, height, nrChannels;
-    unsigned char *data = stbi_load("container.jpeg", &width, &height, &nrChannels, 0); 
+    // shader config
+    lightingShader.use();
+    lightingShader.setInt("material.diffuse", 0);
+    lightingShader.setInt("material.specular", 1);
+    lightingShader.setFloat("material.shininess", 32.0f);
 
-    stbi_image_free(data);
 
-    // set up camera
-
-
-    while(!glfwWindowShouldClose(window)) // main render loop, terminates when glfw gets a close signal
-    {
+    while (!glfwWindowShouldClose(window)) { // main render loop, terminates when glfw gets a close signal
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame; 
@@ -175,79 +236,43 @@ int main()
         processInput(window); // call the process input function every frame
         
         // render functions
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // activate the shader
         lightingShader.use();
 
-        lightingShader.setVec3("objectColor",glm::vec3(1.0f,0.5,0.31f));
-        lightingShader.setVec3("lightColor",glm::vec3(1.0f,1.0f,1.0f));
-        lightingShader.setVec3("lightPos",lightPos);
+        glm::vec3 lightColor = glm::vec3(1.0f,1.0f,1.0f);
+        glm::vec3 ambientColor = lightColor * glm::vec3(0.2f); 
+        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); 
+
+
+        lightingShader.setVec3("dirLight.ambient",ambientColor);
+        lightingShader.setVec3("dirLight.diffuse",diffuseColor);
+        lightingShader.setVec3("dirLight.specular",lightColor);
+        lightingShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f); 
+
+        // view/projection transformations
         lightingShader.setVec3("viewPos", camera.Position);
-
-        // camera/view transformation
+        glm::mat4 projectionMat = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); // 45 degree field of view, 800x600 aspect ratio, 0.1 close field, 100 far field
         glm::mat4 viewMat = camera.GetViewMatrix();
-
+        lightingShader.setMat4("projection",projectionMat);
         lightingShader.setMat4("view",viewMat);
 
-        // create projection matrix
-        glm::mat4 projectionMat;
-        projectionMat = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); // 45 degree field of view, 800x600 aspect ratio, 0.1 close field, 100 far field
-
-        lightingShader.setMat4("projection",projectionMat);
-
-        // multiple cubes, all in a bit different spot
-        glm::vec3 cubePositions[] = {
-            glm::vec3( 0.0f,  0.0f,  0.0f), 
-            glm::vec3( 2.0f,  5.0f, -15.0f), 
-            glm::vec3(-1.5f, -2.2f, -2.5f),  
-            glm::vec3(-3.8f, -2.0f, -12.3f),  
-            glm::vec3( 2.4f, -0.4f, -3.5f),  
-            glm::vec3(-1.7f,  3.0f, -7.5f),  
-            glm::vec3( 1.3f, -2.0f, -2.5f),  
-            glm::vec3( 1.5f,  2.0f, -2.5f), 
-            glm::vec3( 1.5f,  0.2f, -1.5f), 
-            glm::vec3(-1.3f,  1.0f, -1.5f)  
-        };
-
-        // render the cube, repeat 10 times with each
         glBindVertexArray(cubeVAO);
 
-        for (int i = 0; i < sizeof(cubePositions)/sizeof(cubePositions[0]); i++) {
-            // model transformation matrix
+        // render each part of the robot
+        for (int i = 0; i < parts.size(); i++) {
             glm::mat4 modelMat = glm::mat4(1.0f);
-            modelMat = glm::translate(modelMat,cubePositions[i]);
-            float angle = 20.0f * i;
-            modelMat = glm::rotate(modelMat,glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-
-            if (i % 3 == 0) {
-                modelMat = glm::rotate(modelMat,(float)glfwGetTime(), glm::vec3(1.0f, 0.0f, 0.0f));
-            }
+            modelMat = glm::translate(modelMat,parts[i].position);
+            modelMat = glm::rotate(modelMat,parts[i].rotation,parts[i].rotationAxis);
+            modelMat = glm::scale(modelMat,parts[i].dimensions);
 
             lightingShader.setMat4("model",modelMat);
 
+            lightingShader.setVec3("color",parts[i].color); // make the light cube have the color of the light
 
             glDrawArrays(GL_TRIANGLES,0,36);
         }
-        // draw the cube objects
-        glDrawArrays(GL_TRIANGLES,0,36);
-
-
-
-        // draw the light source marker
-        lightCubeShader.use();
-        lightCubeShader.setVec3("lightColor",glm::vec3(1.0f,1.0f,1.0f)); // make the light cube have the color of the light
-
-        glm::mat4 modelMat = glm::mat4(1.0f);
-        modelMat = glm::translate(modelMat, lightPos);
-        modelMat = glm::scale(modelMat,glm::vec3(0.2f));
-        lightCubeShader.setMat4("view",viewMat);
-        lightCubeShader.setMat4("projection",projectionMat);
-        lightCubeShader.setMat4("model",modelMat);
-        glBindVertexArray(lightCubeVAO);
-        glDrawArrays(GL_TRIANGLES,0,36);
-
 
         // swap the buffers and poll IO event
         glfwSwapBuffers(window);
@@ -268,7 +293,6 @@ int main()
 
     // de-allocate all resources after they're done
     glDeleteVertexArrays(1, &cubeVAO);
-    glDeleteVertexArrays(1, &lightCubeVAO);
     glDeleteBuffers(1, &VBO);
 
     glfwTerminate(); // clean up allocated glfw resources
--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,33 +1,57 @@
 #version 330 core
 
-out vec4 FragColor;
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
 
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
+struct DirLight {
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform DirLight dirLight;
+
+uniform Material material;
 uniform vec3 viewPos;
+
+uniform vec3 color;
 
 in vec3 Normal;
 in vec3 FragPos;
 
+out vec4 FragColor;
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+
+
 void main()
 {
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
-
-    vec3 norm = normalize(Normal); // a lot of norms going on here
-    vec3 lightDir = normalize(lightPos - FragPos); // direction between this fragment and light
-
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-
-    float specularStrength = 0.5;
+    // properties
+    vec3 norm = normalize(Normal);
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm); 
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor; 
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
-    // result = (Normal+vec3(1.0,1.0,1.0))/2;
+    // directional lighting
+    vec3 result = CalcDirLight(dirLight, norm, viewDir);
+
     FragColor = vec4(result, 1.0);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // combine results
+    vec3 ambient = light.ambient * color;
+    vec3 diffuse = light.diffuse * diff * color;
+    vec3 specular = light.specular * spec * color;
+    return (ambient + diffuse + specular);
 }
